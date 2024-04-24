0760ca3a92e1f63789240f92c4aca9f0efe14480
SREERAM A S <sreeram292004@gmail.com>
2024-04-24 20:49:10 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# biteline
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/sreedev/biteline/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sreedev.biteline
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
